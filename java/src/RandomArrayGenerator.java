82c9ca201265755cb845ed364e066678df8768fe
koteho <pavel.kryvonis@gmail.com>
2013-11-25 23:54:50 +0200

    Remove unnecessary second duplicate method from RandomArrayGenerator.class

--- java/src/RandomArrayGenerator.java
+++ java/src/RandomArrayGenerator.java
@@ -1,10 +1,10 @@
 import java.util.Random;
 
 public class RandomArrayGenerator {
-    public static int[] arrayOne = randomArraysOne();
-    public static int[] arrayTwo = randomArraysTwo();
+    public static int[] arrayOne = randomArrays();
+    public static int[] arrayTwo = randomArrays();
 
-    private static int[] randomArraysOne(){
+    private static int[] randomArrays(){
 
         int[] arrayOne;
         Random rand = new Random();
@@ -20,20 +20,4 @@ public class RandomArrayGenerator {
         return arrayOne;
     }
 
-    private static int[] randomArraysTwo(){
-
-        int[] arrayTwo;
-        Random rand = new Random();
-        int randomNumber = 0;
-            while (randomNumber == 0){
-                randomNumber = rand.nextInt(20);
-            }
-        arrayTwo = new int[randomNumber];
-        for(int elementOfArrayTwo = 0; elementOfArrayTwo < arrayTwo.length; elementOfArrayTwo++){
-            Random random = new Random();
-            int randomNumberForArrayTwo = random.nextInt(20) - 10;
-            arrayTwo[elementOfArrayTwo] = randomNumberForArrayTwo;
-        }
-        return arrayTwo;
-    }
 }
